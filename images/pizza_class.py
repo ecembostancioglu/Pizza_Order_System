fe6b9725cb57c00812acded8a30fc90818e62435
ecembostancioglu <ecembostancioglu21@gmail.com>
2023-03-05 12:14:30 +0300

    Added pizza class

---
+++ images/pizza_class.py
@@ -0,0 +1,12 @@
+class Pizza:
+    def __init__(self,isim,aciklama,fiyat):
+        self.isim=isim
+        self.aciklama=aciklama
+        self.fiyat=fiyat
+    
+
+    def get_description(self):
+        return self.aciklama
+    
+    def get_cost(self):
+        return self.fiyat
