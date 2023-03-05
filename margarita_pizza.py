c18713a8ca943ca9e86d60c21e53ebfadf70a042
ecembostancioglu <ecembostancioglu21@gmail.com>
2023-03-05 12:35:32 +0300

    Added all pizza class types

---
+++ margarita_pizza.py
@@ -0,0 +1,7 @@
+from pizza_class import Pizza
+
+
+class MargaritaPizza(Pizza):
+    
+    def __init__(self, isim, aciklama, fiyat):
+        super().__init__(isim, aciklama, fiyat)
---
+++ sade_pizza.py
@@ -0,0 +1,7 @@
+from pizza_class import Pizza
+
+
+class SadePizza(Pizza):
+    
+    def __init__(self, isim, aciklama, fiyat):
+        super().__init__(isim, aciklama, fiyat)
