9f389ffb4c9d61989591fbed68efcf35245c1fa5
ecembostancioglu <ecembostancioglu21@gmail.com>
2023-03-08 02:13:15 +0300

    Changed class names and deleted name properties of classes

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from tkinter import *
 from PIL import Image,ImageTk
 
-from decorator import Decorator
 
 from pizza_types.classic_pizza import ClassicPizza
 from pizza_types.margherita_pizza import MargheritaPizza
-from pizza_types.sade_pizza import SadePizza
-from pizza_types.turk_pizza import TurkPizza
+from pizza_types.base_pizza import BasePizza
+from pizza_types.turkish_pizza import TurkishPizza
 from sauce_types.cheese import Cheese
 from sauce_types.corn import Corn
 from sauce_types.meat import Meat
@@ -19,10 +18,6 @@ def main():
     print('Menuyu yazdirir')
 
 main()
-
-def addToBasket():
-    total=int(button1 + button2 + button3 + button4 + button5 + button6 + button7 + button8 + button9)
-    print(total)  
 
 form = Tk()
 form.title('Pizza Dünyası')
@@ -38,14 +33,13 @@ pizza_selection=Label(form,text='Lütfen Bir Pizza Tabanı Seçiniz',font='Times
 menu_title.pack()
 pizza_selection.pack()
 
-klasik_pizza=Image.open('images/classic_pizza.png')
-pizza1_resize=klasik_pizza.resize((110,110))
+classic_pizza=Image.open('images/classic_pizza.png')
+pizza1_resize=classic_pizza.resize((110,110))
 pizza1=ImageTk.PhotoImage(pizza1_resize)
 label1=Label(form,image=pizza1).place(x=40,y=80)
 
-classic=ClassicPizza(name='Klasik Pizza',description='Peynirli Pizza',price='200 TL')
+classic=ClassicPizza()
 
-pizza1_name=Label(form,text=classic.name,font='Times 14 bold italic',fg='darkred').place(x=160,y=100)
 pizza1_description=Label(form,text=classic.description,font='Times 12 italic',fg='black').place(x=160,y=125)
 pizza1_price=Label(form,text=classic.price,font='Times 12 italic',fg='black').place(x=160,y=150)
 
@@ -58,52 +52,49 @@ pizza2_resize=margherita_pizza.resize((110,110))
 pizza2=ImageTk.PhotoImage(pizza2_resize)
 label2=Label(form,image=pizza2).place(x=410,y=80)
 
-margherita=MargheritaPizza(name='Margarita Pizza',description='Peynir ve Domatesli Pizza',price='300 TL')
+margherita=MargheritaPizza()
 
-pizza2_name=Label(form,text=margherita.name,font='Times 14 bold italic',fg='darkred').place(x=530,y=100)
 pizza2_description=Label(form,text=margherita.description,font='Times 12 italic',fg='black').place(x=530,y=125)
 pizza2_price=Label(form,text=margherita.price,font='Times 12 italic',fg='black').place(x=530,y=150)
 
 button2=Button(form,text='+',background='green',padx=10,command=margherita.get_cost).place(x=590,y=150)
 
 
-sade_pizza=Image.open('images/sade_pizza.png')
-pizza3_resize=sade_pizza.resize((110,110))
+base_pizza=Image.open('images/base_pizza.png')
+pizza3_resize=base_pizza.resize((110,110))
 pizza3=ImageTk.PhotoImage(pizza3_resize)
 label3=Label(form,image=pizza3).place(x=40,y=220)
 
-sade=SadePizza(name='Sade Pizza',description='Peynir ve Domates Soslu Pizza',price='250 TL')
+base=BasePizza()
 
-pizza3_name=Label(form,text=sade.name,font='Times 14 bold italic',fg='darkred').place(x=160,y=240)
-pizza3_description=Label(form,text=sade.description,font='Times 12 italic',fg='black').place(x=160,y=265)
-pizza3_price=Label(form,text=sade.price,font='Times 12 italic',fg='black').place(x=160,y=290)
+pizza3_description=Label(form,text=base.description,font='Times 12 italic',fg='black').place(x=160,y=265)
+pizza3_price=Label(form,text=base.price,font='Times 12 italic',fg='black').place(x=160,y=290)
 
-button3=Button(form,text='+',background='green',padx=10,command=sade.get_cost).place(x=220,y=290)
+button3=Button(form,text='+',background='green',padx=10,command=base.get_cost).place(x=220,y=290)
 
 
-turk_pizza=Image.open('images/turk_pizza.png')
-pizza4_resize=turk_pizza.resize((110,110))
+turkish_pizza=Image.open('images/turkish_pizza.png')
+pizza4_resize=turkish_pizza.resize((110,110))
 pizza4=ImageTk.PhotoImage(pizza4_resize)
 label4=Label(form,image=pizza4).place(x=410,y=220)
 
-turk=TurkPizza(name='Türk Pizza',description='Peynir ve Sucuklu Pizza',price='400 TL')
+turkish=TurkishPizza()
 
-pizza4_name=Label(form,text=turk.name,font='Times 14 bold italic',fg='darkred').place(x=530,y=240)
-pizza4_description=Label(form,text=turk.description,font='Times 12 italic',fg='black').place(x=530,y=265)
-pizza4_price=Label(form,text=turk.price,font='Times 12 italic',fg='black').place(x=530,y=290)
+pizza4_description=Label(form,text=turkish.description,font='Times 12 italic',fg='black').place(x=530,y=265)
+pizza4_price=Label(form,text=turkish.price,font='Times 12 italic',fg='black').place(x=530,y=290)
 
-button4=Button(form,text='+',background='green',padx=10,command=turk.get_cost).place(x=590,y=290)
+button4=Button(form,text='+',background='green',padx=10,command=turkish.get_cost).place(x=590,y=290)
 
 
-pizza_sauce_secimi=Label(form,text='Lütfen Bir Pizza Sosu Seçiniz',font='Times 12 italic').place(x=270,y=350)
+pizza_sauce_selection=Label(form,text='Lütfen Bir Pizza Sosu Seçiniz',font='Times 12 italic').place(x=270,y=350)
 
 sauce_1=Image.open('images/olive.png')
 sauce1_resize=sauce_1.resize((120,80))
 olive_image=ImageTk.PhotoImage(sauce1_resize)
 label5=Label(form,image=olive_image).place(x=50,y=400)
 
-olive=Olive(name='Zeytin',price='10 TL')
-olive_name=Label(form,text=olive.name,font='Times 12 italic',fg='black').place(x=175,y=420)
+olive=Olive()
+olive_description=Label(form,text=olive.description,font='Times 12 italic',fg='black').place(x=175,y=420)
 olive_price=Label(form,text=olive.price,font='Times 12 italic',fg='black').place(x=175,y=440)
 
 button5=Button(form,text='+',background='green',padx=10,command=olive.get_cost).place(x=180,y=465)
@@ -113,8 +104,8 @@ sauce2_resize=sauce_2.resize((120,80))
 mushroom_image=ImageTk.PhotoImage(sauce2_resize)
 label6=Label(form,image=mushroom_image).place(x=275,y=400)
 
-mushroom=Mushroom(name='Mantarlar',price='40 TL')
-mushroom_name=Label(form,text=mushroom.name,font='Times 12 italic',fg='black').place(x=400,y=420)
+mushroom=Mushroom()
+mushroom_description=Label(form,text=mushroom.description,font='Times 12 italic',fg='black').place(x=400,y=420)
 mushroom_price=Label(form,text=mushroom.price,font='Times 12 italic',fg='black').place(x=400,y=440)
 
 button6=Button(form,text='+',background='green',padx=10,command=mushroom.get_cost).place(x=405,y=465)
@@ -125,12 +116,11 @@ sauce3_resize=sauce_3.resize((120,80))
 cheese_image=ImageTk.PhotoImage(sauce3_resize)
 label7=Label(form,image=cheese_image).place(x=500,y=400)
 
-cheese=Cheese(name='Keçi Peyniri',price='50 TL')
-cheese_name=Label(form,text=cheese.name,font='Times 12 italic',fg='black').place(x=625,y=420)
+cheese=Cheese()
+cheese_description=Label(form,text=cheese.description,font='Times 12 italic',fg='black').place(x=625,y=420)
 cheese_price=Label(form,text=cheese.price,font='Times 12 italic',fg='black').place(x=625,y=440)
 
 button6=Button(form,text='+',background='green',padx=10,command=cheese.get_cost).place(x=630,y=465)
-
 
 
 sauce_4=Image.open('images/meat.png')
@@ -138,9 +128,9 @@ sauce4_resize=sauce_4.resize((120,80))
 meat_image=ImageTk.PhotoImage(sauce4_resize)
 label8=Label(form,image=meat_image).place(x=50,y=550)
 
-meat=Meat(name='Et',price='100 TL')
+meat=Meat()
 
-meat_name=Label(form,text=meat.name,font='Times 12 italic',fg='black').place(x=175,y=570)
+meat_description=Label(form,text=meat.description,font='Times 12 italic',fg='black').place(x=175,y=570)
 meat_price=Label(form,text=meat.price,font='Times 12 italic',fg='black').place(x=175,y=590)
 
 
@@ -153,8 +143,8 @@ sauce5_resize=sauce_5.resize((120,80))
 onion_image=ImageTk.PhotoImage(sauce5_resize)
 label9=Label(form,image=onion_image).place(x=275,y=550)
 
-onion=Onion(name='Soğan',price='25 TL')
-onion_name=Label(form,text=onion.name,font='Times 12 italic',fg='black').place(x=400,y=570)
+onion=Onion()
+onion_description=Label(form,text=onion.description,font='Times 12 italic',fg='black').place(x=400,y=570)
 onion_price=Label(form,text=onion.price,font='Times 12 italic',fg='black').place(x=400,y=590)
 
 
@@ -167,14 +157,14 @@ sauce6_resize=sauce_6.resize((120,80))
 corn_image=ImageTk.PhotoImage(sauce6_resize)
 label10=Label(form,image=corn_image).place(x=500,y=550)
 
-corn=Corn(name='Mısır',price='30 TL')
-corn_name=Label(form,text=corn.name,font='Times 12 italic',fg='black').place(x=625,y=570)
+corn=Corn()
+corn_description=Label(form,text=corn.description,font='Times 12 italic',fg='black').place(x=625,y=570)
 corn_price=Label(form,text=corn.price,font='Times 12 italic',fg='black').place(x=625,y=590)
 
 
 button9=Button(form,text='+',background='green',padx=10,command=corn.get_cost).place(x=630,y=615)
 
-
+#text=Entry(form,invcmd=addBasket).place(x=650,y=700)
 
 form.mainloop()
 
--- sauce_types/olive.py
+++ sauce_types/olive.py
@@ -3,5 +3,7 @@ from decorator import Decorator
 
 class Olive(Decorator):
     
-    def __init__(self, name, price):
-        super().__init__(name, price)
+     def __init__(self,component):
+        super().__init__(component)
+        self.description='Zeytin'
+        self.price=6
